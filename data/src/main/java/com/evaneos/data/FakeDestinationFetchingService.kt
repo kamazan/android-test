b05c4b0eb61e4b6d85d2f5cd0beae4c8f93ec2eb
macaroff <caroff.marc.alexandre@gmail.com>
2021-08-03 16:27:27 +0200

    Added random delay for data fetches

--- data/src/main/java/com/evaneos/data/FakeDestinationFetchingService.kt
+++ data/src/main/java/com/evaneos/data/FakeDestinationFetchingService.kt
@@ -3,13 +3,21 @@ package com.evaneos.data
 import com.evaneos.data.factory.DataFactory
 import com.evaneos.data.model.Destination
 import com.evaneos.data.model.DestinationDetails
+import kotlinx.coroutines.delay
+import kotlin.random.Random
 
 class FakeDestinationFetchingService : DestinationFetchingService {
-    override suspend fun getDestinations() : List<Destination> {
-        return DataFactory.createDestinationList()
+    override suspend fun getDestinations() : List<Destination>  = withFakeDelay {
+        DataFactory.createDestinationList()
     }
 
-    override suspend fun getDestinationDetails(id: Long) : DestinationDetails {
-        return DataFactory.getDestinationDetails(id)
+    override suspend fun getDestinationDetails(id: Long) : DestinationDetails = withFakeDelay{
+        DataFactory.getDestinationDetails(id)
+    }
+
+    private suspend fun <T> withFakeDelay(action: suspend () -> T): T {
+        // Fake network delay
+        delay(Random.nextLong(200, 5000))
+        return action()
     }
 }
