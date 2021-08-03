345317aad4c06084d5fd54669d37e76850041b1c
macaroff <caroff.marc.alexandre@gmail.com>
2021-08-03 16:47:18 +0200

    Added potential errors

--- data/src/main/java/com/evaneos/data/FakeDestinationFetchingService.kt
+++ data/src/main/java/com/evaneos/data/FakeDestinationFetchingService.kt
@@ -7,11 +7,24 @@ import kotlinx.coroutines.delay
 import kotlin.random.Random
 
 class FakeDestinationFetchingService : DestinationFetchingService {
-    override suspend fun getDestinations() : List<Destination>  = withFakeDelay {
-        DataFactory.createDestinationList()
+    override suspend fun getDestinations() : List<Destination> = withFakeDelay {
+        val number = Random.nextInt(1, 5)
+        if (number == 1) {
+            throw Exception("Oops, something went wrong")
+        }
+
+        if (number == 2) {
+            emptyList()
+        } else {
+            DataFactory.createDestinationList()
+        }
     }
 
-    override suspend fun getDestinationDetails(id: Long) : DestinationDetails = withFakeDelay{
+    override suspend fun getDestinationDetails(id: Long) : DestinationDetails = withFakeDelay {
+        if (id == 170L) {
+            throw Exception("Oops, something went wrong")
+        }
+
         DataFactory.getDestinationDetails(id)
     }
 
